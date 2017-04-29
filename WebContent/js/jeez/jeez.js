1780c37780e6aab0a210ac47f496ee6caf39fd56
tutanck <tutanck@gmail.com>
2017-04-29 18:53:45 +0200

    wip

--- WebContent/js/jeez/jeez.js
+++ WebContent/js/jeez/jeez.js
@@ -28,6 +28,28 @@ var JZAPPROUTES;
 	function routes_(routes){JZAPPROUTES=JSON.parse(routes);if(debug)clog(JSON.stringify(JZAPPROUTES))} 
 })();//true ->debug
 
+
+function check(url,input){
+	var fn="check: ";
+
+	if( isFalsy(field) || isFalsy(field.name) )
+		throw JEEZ+fn+"Input to check is not defined";
+	
+	if( isFalsy(field) || isFalsy(field.name) )
+		throw JEEZ+fn+"Input to check is unknown";
+
+	if( isEmpty(field.value) )	return status(1,-1);
+	
+	if( isEmpty(field.value) )	return status(1,-1);
+	
+	if( isEmpty(field.value) )	return status(1,-1);
+
+	return status(0);
+
+	function status(status,cause){ return {"status":status,"cause":cause}; }
+}
+
+
 connect('signup')
 function connect(url,response_processor,fields,error_manager,async=true){
 	var fn="connect: ";
@@ -37,18 +59,20 @@ function connect(url,response_processor,fields,error_manager,async=true){
 	for(let fi in new Set(fields)){
 		let field = fields[fi];
 		if( isEmpty(field) || isEmpty(field.name) || isEmpty(field.value) ){
-			clog(fn,"Warning: the tab 'fields' contains an empty field");
+			clog(fn,"Warning: Array 'fields' contains an empty field");
 			continue;
 		}
 		index.set(field.name,field);
 		data[field.name]=field.value;
 	}
+
 	clog(fn,"data = '"+JSON.stringify(data)+"'");
+
 	try{
 		send(url,response_processor,error_manager,data);
 	}catch(e){
 		if(e instanceof JEEZInvalidParameterException)
-			return {"field":index.get(e.param),"invalidity":e.invalidity};
+			return {"field":index.get(e.fparamName),"invalidity":e.invalidity};
 	}
 }
 
@@ -59,74 +83,70 @@ function send(url,response_processor,data={},error_manager,async=true){
 	if(!jz_configured_()) 
 		throw JEEZ+fn+"JEEZ routes not loaded";
 
-	url=jz_slash_bf(url,0);
-	var config = JZAPPROUTES[url];
-	clog(fn,"route config: ",url,"->",JSON.stringify(config)); 
+	var config=getConfig(url);
+	clog(fn,"route config: ",url,"->",JSON.stringify(config));
 
 	check_params(config.expin,true);
 	check_params(config.optin);
 
-	if(JZAPPSERVER)
-		url=JZAPPSERVER+jz_unslash(url,0);
-	else
-		url=jz_unslash(url,0);
-
-	jz_send_ajax_request_(url,response_processor,error_manager,data,config.httpm,"text",async);
+	jz_send_ajax_request_(getUrl(url),response_processor,error_manager,data,config.httpm,"text",async);
 
 
 	/*INTERNAL TOOLS*/
 
-	function check_params(params,expected=false){
-		for(let x in params) {
-			let param=params[x];
-			let fname=param.name;//formal name
-			let ftype=getJSFType(param.type);//formal type
-			let frules=param.rules;//formal rules
-
-			clog (JSON.stringify(param));
-
-			let effval = data[fname]; //effective value
-
-			if(isEmpty(effval))
-				if(expected) 
-					throw new JEEZInvalidParameterException(fname,-1,JEEZ+fn+"Missing expected parameter '"+fname+"'");
-				else return;
-
-			if(!ftype === typeof effval)
-				throw new JEEZInvalidParameterException(fname,0,JEEZ+fn+"Incompatible type for parameter '"+fname+"', expected was "+ftype);
-
-			for(let ri in frules){
-				let frule = frules[ri];
-				if(!new RegExp("\\b"+frule+"\\b").test(effval))
-					throw new JEEZInvalidParameterException(fname,1,JEEZ+fn+"Parameter '"+fname+"' does not match the rule '"+frule+"'");
-			}
+	/**
+	 * check all params */
+	function check_params(params,expected=false){ 
+		for(let pi in params){
+			let fparam = params[pi]; //formal param def
+			valid_(fparam,data[fparam.name],expected); 
 		}
+	}
+}
 
 
-		function JEEZInvalidParameterException(param,invalidity,message) {
-			this.name = "JEEZInvalidParameterException";
-			this.param=param;
-			this.invalidity = invalidity; //-1:missing , 0:typing , 1:ruling
-			this.message = (message || "");
-		}
-		JEEZInvalidParameterError.prototype = new Error();
+
+function valid_(formal,effval,expected){
+	var fn="valid_: ";
+
+	if(isFalsy(formal))
+		throw JEEZ+fn+"Empty formal parameter definition.";
+
+	clog (fn,"formal =",JSON.stringify(formal));
+
+	let fname=formal.name;
+	let ftype=getJSFType(formal.type);
+	let frules=formal.rules;
+
+	if(isEmpty(effval))
+		if(expected) 
+			throw new JEEZInvalidParameterException(fname,-1,JEEZ+fn+"Missing expected parameter '"+fname+"'");
+		else return;
+
+	if(!ftype === typeof effval)
+		throw new JEEZInvalidParameterException(fname,0,JEEZ+fn+"Incompatible type for parameter '"+fname+"', expected was "+ftype);
+
+	for(let ri in frules){
+		let frule = frules[ri];
+		if(!new RegExp("\\b"+frule+"\\b").test(effval))
+			throw new JEEZInvalidParameterException(fname,1,JEEZ+fn+"Parameter '"+fname+"' does not match the rule '"+frule+"'");
 	}
 
 
 	/**
 	 * Get JavaScript Formal Type */
-	function getJSFType(intType){
-		switch (intType) {
-		case 0:return "string";
+	function getJSFType(intType){ 
+		switch (intType) { 
+		case 0:return "string"; 
 		case 1:return "number";
 		case 2:return "number";
-		case 3:return "number";
-		case 4:return "number";
-		case 5:return "boolean";	
-		default:throw "JEEZError#SNO : InternalTypingError";	
-		}
+		case 3:return "number"; 
+		case 4:return "number"; case 5:return "boolean";	
+		default:throw JEEZ+fn+"JEEZError#SNO : InternalTypingError"; 
+		} 
 	}
 }
+
 
 
 function jz_send_ajax_request_(url,response_processor,error_manager,data={},HTTPMethod="get",dataType="text",async=true){
@@ -155,6 +175,10 @@ function jz_send_ajax_request_(url,response_processor,error_manager,data={},HTTP
 
 function jz_configured_(){return !isEmpty(JZAPPROUTES);}
 
+function getConfig(url){ return JZAPPROUTES[jz_slash_bf(url,0)]; }
+
+function getUrl(url){ return JZAPPSERVER ? JZAPPSERVER+jz_unslash(url,0) : jz_unslash(url,0); }
+
 function jz_unslash(s,i){return (s.charAt(i)==="\/")? [s.slice(0,i),"",s.slice(i+1)].join(''):s;}
 
 function jz_slash_bf(s,i){return (s.charAt(i)!=="\/")? [s.slice(0,i),"\/",s.slice(i)].join(''):s;}
@@ -179,3 +203,11 @@ function isEmpty(value){
 	|| new RegExp('^\\s*$').test(value)
 	|| value == "null"
 }
+
+function JEEZInvalidParameterException(fparamName,invalidity,message) {
+	this.name = "JEEZInvalidParameterException";
+	this.fparamName=fparamName;
+	this.invalidity = invalidity; //-1:missing , 0:typing , 1:ruling
+	this.message = (message || "");
+}
+JEEZInvalidParameterException.prototype = new Error();
