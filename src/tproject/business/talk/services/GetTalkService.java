ced2dc7676c2a9dc402b4017105ef7b63996a69a
Tutanck <tutanck@gmail.com>
2017-06-28 09:37:14 +0200

    talk

---
+++ src/tproject/business/talk/services/GetTalkService.java
@@ -0,0 +1,58 @@
+package tproject.business.talk.services;
+
+import org.json.JSONObject;
+
+import com.aj.jeez.gate.representation.annotations.Param;
+import com.aj.jeez.gate.representation.annotations.Params;
+import com.aj.jeez.gate.representation.annotations.WebService;
+import com.aj.jeez.jr.JR;
+import com.aj.jeez.jr.exceptions.AbsentKeyException;
+import com.aj.jeez.jr.exceptions.InvalidKeyException;
+import com.aj.jeez.regina.THINGS;
+import com.mongodb.DBObject;
+
+import tproject.business.need.services.core.NeedCore;
+import tproject.conf.servletspolicy.OnlinePostServlet;
+import tproject.tools.db.DBException;
+import tproject.tools.services.Response;
+import tproject.tools.services.ServiceCodes;
+import tproject.tools.services.ShouldNeverOccurException;
+
+/**
+ * 
+ * @author AJoan
+ * Post are need search representation */
+public class GetTalkService extends NeedCore{
+	public final static String url="/need/up";
+
+	public final static String _nid="nid";
+	
+	public final static String _entity="entity";
+	public final static String _need="need";
+
+	/**
+	 * update user's profile
+	 * @param params
+	 * @return
+	 * @throws DBException 
+	 * @throws ShouldNeverOccurException 
+	 * @throws AbsentKeyException */
+	@WebService(value=url,policy = OnlinePostServlet.class,
+			requestParams=@Params( 
+					value={
+							@Param(value=_nid)
+					}))
+	public static JSONObject get(
+			JSONObject params
+			) throws DBException, ShouldNeverOccurException, AbsentKeyException, InvalidKeyException {	
+
+		DBObject need = THINGS.getOne(
+				JR.renameKeys(JR.slice(params,_nid),_nid+"->_id")
+				,needdb);
+
+		return need == null ? 
+				Response.issue(ServiceCodes.UNKNOWN_RESOURCE)
+				:
+					Response.reply(need.put(_entity,_need));
+	}
+}
---
+++ src/tproject/business/talk/services/TalkListService.java
@@ -0,0 +1,54 @@
+package tproject.business.talk.services;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.aj.jeez.gate.representation.annotations.WebService;
+import com.aj.jeez.jr.JR;
+import com.aj.jeez.jr.exceptions.AbsentKeyException;
+import com.aj.jeez.jr.exceptions.InvalidKeyException;
+import com.aj.jeez.regina.THINGS;
+import com.mongodb.DBCursor;
+
+import tproject.business.need.db.NeedDB;
+import tproject.business.need.services.core.NeedCore;
+import tproject.conf.servletspolicy.OnlinePostServlet;
+import tproject.tools.db.DBException;
+import tproject.tools.services.Response;
+import tproject.tools.services.ShouldNeverOccurException;
+
+/**
+ * 
+ * @author AJoan
+ * Post are need search representation */
+public class TalkListService extends NeedCore{
+	public final static String url="/need/up";
+
+	public final static String _nid="nid";
+
+	/**
+	 * update user's profile
+	 * @param params
+	 * @return
+	 * @throws DBException 
+	 * @throws ShouldNeverOccurException 
+	 * @throws AbsentKeyException */
+	@WebService(value=url,policy = OnlinePostServlet.class)
+	public static JSONObject list(
+			JSONObject params
+			) throws DBException, ShouldNeverOccurException, AbsentKeyException, InvalidKeyException {	
+
+		DBCursor dbc = THINGS.get(
+				JR.renameKeys(JR.slice(params,_nid),_nid+"->_id")
+				,needdb);
+
+		JSONArray jar = new JSONArray();
+		while(dbc.hasNext())
+			jar.put(
+					JR.slice(JR.jsonify(dbc.next()),
+							_nid,NeedDB._title)
+					);
+
+		return Response.reply(jar);
+	}
+}
